b374524d9f2b11474aafb77d34cd06e440578b23
HamamEL <hamam.alsaiity@gmail.com>
2019-11-08 16:43:53 -0800

    more css changes

--- posts.js
+++ posts.js
@@ -1,24 +1,22 @@
 var posts = [MakePost('He allowed himself to be swayed by his conviction that human beings are not born once and for all on the day their mothers give birth to them, but that life obliges them over and over again to give birth to themselves.'),MakePost('The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.'), MakePost('If you would be a real seeker after truth, it is necessary that at least once in your life you doubt, as far as possible, all things '), ];
-//this function will take the text and return it with pu
+//this function will return the post object which has text and the date
 function MakePost(text){
 return {
 	text: text,
 	date: new Date()
 }
-}
-console.log(posts)
+}// this function will display posts in the posts div
 function displayPosts(){
-		$('.posts').html('<p>Your Posts</p>')
-		posts.forEach(function(post,index){
-		var postdiv = $('<div class= "oldposts"></div>')
-		var deletebtn = $ ('<div><button class= "delete">delete</button><div>')
-		deletebtn.on('click',function(){
-			console.log(index)
-			posts.splice(posts.indexOf(post),1)
-			console.log(posts)
-			$(this).parent().remove()
-		})
-		console.log(deletebtn)
+		$('.posts').html('<p>Your Posts</p>') //change html to empty old posts
+		posts.forEach(function(post,index){ //loop over the array
+			var postdiv = $('<div class= "oldposts"></div>')
+			var deletebtn = $ ('<div><button class= "delete">delete</button><div>')
+			deletebtn.on('click',function(){ //add delete button
+				console.log(index)
+				posts.splice(posts.indexOf(post),1) //remove post by finding index in posts array
+				console.log(posts)
+				$(this).parent().remove()
+			})
 		postdiv.append(post.text)
 		postdiv.append(deletebtn)
 		postdiv.append('<div class ="postdate " >'+post.date.toDateString()+ '</div>')
@@ -27,31 +25,33 @@ function displayPosts(){
 	})
 }
 displayPosts()
+
+//this function adds posts to the posts array
 function addPost(){
 	var input = $('#text').val()
 	if(input !== ''){
-	var post = MakePost(input)
-	posts.push(post)
-	displayPosts()
-}else{
-	alert('please enter a word')
-}}
-$('#btn').on('click',addPost)
+		var post = MakePost(input)
+		posts.push(post)
+		displayPosts()
+	}else{
+		alert('please enter a word')
+	}}
+$('#btn').on('click',addPost) //add function addpost to button
 
 
-
+//search for posts by text input
 function searchPosts(){
-	var find = $('#text').val()
-	var found = posts.filter(function(elem,index){
+	var find = $('#text').val() //get the text
+	var found = posts.filter(function(elem,index){ //filter to find posts
 		return elem.text.indexOf(find)>=0;
 	})
 	$('.posts').html('<p>search results</p>')
 		found.forEach(function(post,index){
-		var postdiv = $('<div class= "oldposts"></div>')
-		var deletebtn = $ ('<div><button class= "delete">delete</button><div>')
-		deletebtn.on('click',function(){
-			posts.splice(posts.indexOf(post),1)
-			$(this).parent().remove()
+			var postdiv = $('<div class= "oldposts"></div>')
+			var deletebtn = $ ('<div><button class= "delete">delete</button><div>')
+			deletebtn.on('click',function(){
+				posts.splice(posts.indexOf(post),1)
+				$(this).parent().remove()
 		})
 		console.log(deletebtn)
 		postdiv.append(post.text)
